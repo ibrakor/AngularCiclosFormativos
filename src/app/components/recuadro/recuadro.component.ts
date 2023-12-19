c7576025c7802a0c959d341b0276bba56a8af5ce
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2023-12-19 12:31:39 +0100

    ciclos formativos

--- src/app/components/recuadro/recuadro.component.ts
+++ src/app/components/recuadro/recuadro.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {TableComponent} from "../table/table.component";
+import {CicloformativoService} from "../../services/cicloformativo.service";
 
 @Component({
   selector: 'app-recuadro',
@@ -11,8 +12,15 @@ import {TableComponent} from "../table/table.component";
   styleUrl: './recuadro.component.css'
 })
 export class RecuadroComponent {
+  constructor(private cfService: CicloformativoService) {
+  }
   @Input() cabecerasRec: any[] = []
   @Input() datosRec: any[] = []
   @Input() titulorecuadro: string = ""
   @Input() textoboton: string = ""
+
+  alerta() {
+    this.cfService.cicloAlert(this.titulorecuadro)
+  }
+
 }
